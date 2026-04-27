8cba797669455369e7454ef595bf2a7573f87cdc
20guix09 <guilhermelimadejesus.estudos@gmail.com>
2026-04-26 21:48:22 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+const slides = document.querySelectorAll(".slide");
+const btnEsquerda = document.querySelector(".seta.esquerda");
+const btnDireita = document.querySelector(".seta.direita");
+
+let index = 0;
+
+function mostrarSlide(i) {
+  slides.forEach(slide => slide.classList.remove("ativo"));
+  slides[i].classList.add("ativo");
+}
+
+btnDireita.addEventListener("click", () => {
+  index++;
+  if (index >= slides.length) index = 0;
+  mostrarSlide(index);
+});
+
+btnEsquerda.addEventListener("click", () => {
+  index--;
+  if (index < 0) index = slides.length - 1;
+  mostrarSlide(index);
+});
+
+setInterval(() => {
+  index++;
+  if (index >= slides.length) index = 0;
+  mostrarSlide(index);
+}, 4000);
