01e505bb8219789dce10095447e5938aa55ddb94
20guix09 <guilhermelimadejesus.estudos@gmail.com>
2026-04-28 21:40:19 -0300

    ajuste na logo

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 const slides = document.querySelectorAll(".sobre-imagem .slide");
 const slidesFullscreen = document.querySelectorAll("#fullscreen .slide");
 
-const btnsEsquerda = document.querySelectorAll(".seta.esquerda");
-const btnsDireita = document.querySelectorAll(".seta.direita");
 
 const fullscreen = document.getElementById("fullscreen");
 const btnFechar = document.querySelector(".fechar");
@@ -18,22 +16,7 @@ function mostrarSlide(i) {
   slidesFullscreen[i].classList.add("ativo");
 }
 
-// ===== SETAS (FUNCIONA NOS DOIS)
-btnsDireita.forEach(btn => {
-  btn.addEventListener("click", () => {
-    index++;
-    if (index >= slides.length) index = 0;
-    mostrarSlide(index);
-  });
-});
 
-btnsEsquerda.forEach(btn => {
-  btn.addEventListener("click", () => {
-    index--;
-    if (index < 0) index = slides.length - 1;
-    mostrarSlide(index);
-  });
-});
 
 // ===== AUTO SLIDE
 setInterval(() => {
